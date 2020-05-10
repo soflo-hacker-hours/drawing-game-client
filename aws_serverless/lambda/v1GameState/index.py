b85be28387fb24908a24f3ab473fbcfe7ffb7d9b
Bryan <btbonval@gmail.com>
2020-05-10 03:27:46 -0400

    minor changes in preparation for writing the API code

--- aws_serverless/lambda/v1GameState/index.py
+++ aws_serverless/lambda/v1GameState/index.py
@@ -1,8 +1,23 @@
-#from LambdaDeps.request import get_session
+from LambdaDeps.request import get_session, get_path
+from LambdaDeps.misc import deep_get
 from LambdaDeps.response import response
-#from LambdaDeps.deployment import table_name
+from LambdaDeps.deployment import table_name
+
+def room_data(table, room):
+    pass
+
+def player_status(table, sess):
+    pass
+
+def new_room():
+    pass
 
 def handler(event, context):
-    #table = resource('dynamodb').Table(table_name)
-    #sess = get_session(event, table)
+    table = resource('dynamodb').Table(table_name)
+    room = deep_get(event, 'pathParameters', 'uuid')
+    if not room:
+        return response(404, error="Missing room.")
+
+    sess = get_session(event, table, room)
+
     return response(200, body="{}")
