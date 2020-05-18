1246d191cd3fc1d8cedf4219b452009035f7df93
Bryan <btbonval@gmail.com>
2020-05-18 16:43:41 -0400

    dynamo mock fleshed out better

--- aws_serverless/lambda/tests.py
+++ aws_serverless/lambda/tests.py
@@ -8,12 +8,22 @@ os.environ['AWSS3'] = ''
 from v1GameState import index
 
 class DynamoStub(object):
-    def __init__(self, db):
-        self.db = db
-    def Table(self, other):
-        return self.db
+    def __init__(self, responses):
+        self.responses = responses
 
-def call(db,cookie=None,room=None,event=None):
+    def Table(self, which_table):
+        # Support only a single table, thus ignore parameter
+        return self
+
+    def query(self, *args, **kwargs):
+        # Assume responses are provided in correct call order, return next.
+        return self.responses.pop(0)
+
+    def get(self, Item):
+        # Assume responses are provided in correct call order, return next.
+        return self.responses.pop(0)
+
+def call(responses,cookie=None,room=None,event=None):
     if event is None:
         if room is None:
             room='DEFAULT'
@@ -27,7 +37,7 @@ def call(db,cookie=None,room=None,event=None):
               'Cookie': cookie
             }
     context = {}
-    index.resource = lambda x: DynamoStub(db)
+    index.resource = lambda x: DynamoStub(responses)
     return index.handler(event, context)
 
 class Test(unittest.TestCase):
@@ -40,15 +50,15 @@ class Test(unittest.TestCase):
         return body
 
     def testNoRoom(self):
-        response = call(db={},event={})
+        response = call(responses=[],event={})
         self.assertFalse(response['isBase64Encoded'])
         self.assertEqual(response['statusCode'], 404)
         self.assertEqual(response['headers'], {})
         self.assertIn('room', response['body'])
 
     def testHostNewRoomNoCookie(self):
-        db={}
-        response = call(db=db,room='NewRoom')
+        responses=[]
+        response = call(responses=responses,room='NewRoom')
         body = self._everytime(response)
         self.assertEqual(response['statusCode'], 200)
         body = json.parse(response['body'])
